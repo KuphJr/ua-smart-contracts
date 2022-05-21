1dee35da770f6a2a84aa2be3b8c1f2806585169a
KuphJr <87319522+KuphJr@users.noreply.github.com>
2022-05-21 08:36:39 -0500

    created deployRequester script

--- scripts/deployRequester.ts
+++ scripts/deployRequester.ts
@@ -1,6 +1,12 @@
 import { ethers } from "hardhat"
 
 async function main() {
+  const accounts = await ethers.getSigners();
+
+  const LinkToken = await ethers.getContractFactory('LinkToken')
+  const linkToken = await LinkToken.attach('0x326C977E6efc84E512bB9C30f76E30c160eD06FB')
+
+
   const Requester = await ethers.getContractFactory("Requester");
   const requester = await Requester.deploy(
     // LINK token address
@@ -12,7 +18,7 @@ async function main() {
     // Offeree
     '0x981FC7F035AD33181eD7604f0708c05674395574',
     // IPFS hash
-    'bafybeieomwdf37r6nooiyeoxhoovn3hfob2xfjwch4wy3rj6l5dojeuqle',
+    'bafybeibugaf5qybcpg3z3la665lhbvcrcdzh2bj2zktmue7uvrvachdhxq',
     // Seconds from now until expiration
     999999
   )
@@ -23,6 +29,21 @@ async function main() {
   console.log(`Waiting for transaction to be mined...`)
   const txReceipt = await ethers.provider.waitForTransaction(txHash)
   console.log("Requester contract deployed to:", txReceipt.contractAddress);
+
+  // Initalize offer
+  const tokenTx = await linkToken.approve(
+    ethers.utils.getAddress(txReceipt.contractAddress),
+    BigInt(100)
+  )
+  await ethers.provider.waitForTransaction(tokenTx.hash)
+  const initTx = await requester.initalizeOffer(
+    BigInt(100)
+  )
+  await ethers.provider.waitForTransaction(initTx.hash)
+
+  const OfferRegistry = await ethers.getContractFactory("OfferRegistry")
+  const offerRegistry = await OfferRegistry.attach('0xe62b71D706302C49aeb371e48C12368DE02e6A1a')
+  console.log(`Num offers in registry: ${await offerRegistry.getRegistryLength()}`)
 }
 
 main().catch((error) => {
