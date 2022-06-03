ec9f6360fc3376547e1456a327f1c7e35c4c60fc
KuphJr <87319522+KuphJr@users.noreply.github.com>
2022-06-03 12:13:04 -0500

    Completed 1st draft of contract

--- contracts/UniversalAdapter.sol
+++ contracts/UniversalAdapter.sol
@@ -13,7 +13,7 @@ contract UniversalAdapter is ChainlinkClient {
   // number of nodes allowed to send responses
   uint constant private NUMBER_OF_NODES = 16;
   // number of responses required to fulfill a Round
-  uint constant private RESPONSE_THRESHOLD = 10;
+  uint constant private RESPONSE_THRESHOLD = 16;
   // base reward sent to each node for a request
   uint constant private BASE_REWARD = (REQUEST_COST_IN_JULES / 2) / RESPONSE_THRESHOLD;
   // required amount of gas to use when executing callback
